29c8f8e8b15824eaf76992bc83938123dab5c749
Your Name <you@example.com>
2015-11-14 00:00:20 +0100

    ongoing ng2-store

--- examples/Todo.ts
+++ examples/Todo.ts
@@ -3,6 +3,10 @@ import {Ng2StoreModel} from 'ng2-store';
 
 export class Todo extends Ng2StoreModel {
 
+    id:number;
+    description:string;
+    completed:boolean;
+
     constructor(id: number, description:string, completed: boolean = false) {
         super({id, description, completed});
     }
--- examples/store/CompleteAllAction.ts
+++ examples/store/CompleteAllAction.ts
@@ -1,14 +1,15 @@
 
 import {Ng2StoreAction} from "ng2-store";
-import List = Immutable.List;
+import {List} from 'immutable';
 import {Todo} from "../Todo";
 
 export class CompleteAllAction implements Ng2StoreAction<List<Todo>> {
 
     execute(state:List<Todo>, args:Object) {
-
-
-
+        return state.reduce(
+            (state, todo) => state.push(new Todo(todo.id, todo.description, true)) ,
+            List([])
+        );
     }
 
 }
