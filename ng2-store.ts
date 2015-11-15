ad1a541ddb0a95aa7a6decabdd021b742595a5fd
Your Name <you@example.com>
2015-11-15 22:59:10 +0100

    ongoing ng2-store

--- ng2-store.ts
+++ ng2-store.ts
@@ -1,6 +1,6 @@
 import {Ng2Store} from './src/Ng2Store';
 import{Ng2StoreAction} from './src/Ng2StoreAction';
-import {Ng2StoreModel} from './src/ImmutableModel';
+import {Ng2StoreModel} from './src/Ng2StoreModel';
 
 export {
     Ng2Store,
