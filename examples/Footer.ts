5247ec784775e4cd050d764acae3dd2a2a5fce85
Your Name <you@example.com>
2015-11-15 22:14:02 +0100

    ongoing ng2-store

--- examples/Footer.ts
+++ examples/Footer.ts
@@ -4,19 +4,6 @@ import {Component} from 'angular2/angular2';
     selector: 'todo-footer',
     template: `
         <footer id="footer">
-            <span id="todo-count"><strong class="ng-binding">3 </strong>
-            items left</span>
-            <ul id="filters">
-                <li>
-                    <a href="#" class="selected">All</a>
-                </li>
-                <li>
-                    <a href="#">Active</a>
-                </li>
-                <li>
-                    <a href="#">Completed</a>
-                </li>
-            </ul>
             <button id="clear-completed">Clear completed (3)</button>
         </footer>
     `
