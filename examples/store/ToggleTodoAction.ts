7f97f9a1934613e0f853e236a258620f8e498214
Your Name <you@example.com>
2015-11-15 22:06:00 +0100

    ongoing ng2-store

--- examples/store/ToggleTodoAction.ts
+++ examples/store/ToggleTodoAction.ts
@@ -5,15 +5,10 @@ import {List} from 'immutable';
 
 export class ToggleTodoAction implements Ng2StoreAction<List<Todo>> {
 
-    execute(state:List<Todo>, modifiedTodo:Todo) {
-        return state.reduce( (state,todo) => {
-            if (todo.id === modifiedTodo.id) {
-                return state.push(Todo.from(todo, {completed: !todo.completed}));
-            }
-            else {
-                return state.push(todo);
-            }
-        }, List<Todo>([]));
+    execute(state:List<Todo>, toggled:Todo) {
+        let index = state.indexOf(toggled);
+        return state.set(index, Todo.from(toggled, {completed: !toggled.completed}) );
     }
 
 }
+
