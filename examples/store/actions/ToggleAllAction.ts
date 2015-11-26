b12ce54235b4b6ddc2562004ea513bfa3f306b27
JHades <jhades.dev@gmail.com>
2015-11-26 20:39:29 +0100

    ng2-store middleware ongoing

--- examples/store/actions/ToggleAllAction.ts
+++ examples/store/actions/ToggleAllAction.ts
@@ -6,7 +6,7 @@ import {Injectable} from 'angular2/angular2';
 @Injectable()
 export class ToggleAllAction implements Ng2StoreAction<List<Todo>> {
 
-    execute(state:List<Todo>, args:Object) {
+    execute(state:List<Todo>, args:Object) : List<Todo> {
         return state.reduce(
             (state, todo) => state.push(Todo.from(todo, {completed: !todo.completed}) ),
             List([])
