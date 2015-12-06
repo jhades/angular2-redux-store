e3779a8a036b0a306adbe798f98d1c36e5fc8a34
JHades <jhades.dev@gmail.com>
2015-12-06 16:25:38 +0100

    refactoring to redux ongoing

--- examples/store/ReduxStore.ts
+++ examples/store/ReduxStore.ts
@@ -2,11 +2,19 @@
 import {Observable, EventEmitter} from 'angular2/angular2';
 
 
-export class ReduxStore {
+export abstract class ReduxStore {
 
+    static initialized = false;
     value: Observable = <Observable>new EventEmitter();
 
     constructor(private store) {
+        if (!store) {
+            throw new Error('store cannot be undefined. Make sure to pass the redux store as the only argument of the constructor.');
+        }
+        if (ReduxStore.initialized) {
+            throw new Error('Only one redux store can exist per application.');
+        }
+        ReduxStore.initialized = true;
         store.subscribe(() => this.value.next(this.getState()));
     }
 
