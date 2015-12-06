58e8d375b51688c1a45a0316d5bf1d312d5d9941
JHades <jhades.dev@gmail.com>
2015-12-06 16:21:38 +0100

    refactoring to redux ongoing

--- examples/store/todoReducers.ts
+++ examples/store/todoReducers.ts
@@ -12,10 +12,8 @@ export function todoReducers(state: List<Todo>, action) {
             return state.push(action.newTodo);
         case TOGGLE_TODO:
             let index = state.findIndex((todo) => todo.id === action.todo.id);
-            let toggled = state.get(index);
-
-            //TODO
-
+            let toggled:Todo = state.get(index);
+            return state.set(index, new Todo(toggled.id, toggled.description, !toggled.completed) );
         case DELETE_TODO:
 
             //TODO
