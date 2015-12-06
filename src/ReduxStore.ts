ceb1ce139e4e05bf30004fbdadf0de37f1c0bf60
JHades <jhades.dev@gmail.com>
2015-12-06 17:05:07 +0100

    refactoring to redux ongoing

--- src/ReduxStore.ts
+++ src/ReduxStore.ts
@@ -1,6 +1,23 @@
 
 import {Observable, EventEmitter} from 'angular2/angular2';
 
+/**
+ *
+ * A minimalistic Redux store for Angular 2. This class is meant for demonstration purposes, as you can see there is
+ * not much going on here.
+ *
+ * This class is meant to be sub-classed per project, and a redux store needs to be passed in the constructor.
+ *
+ * This class then needs to be passed to the root bootstrap call of the application, so that the store can be
+ * injected in any part of the application that needs it.
+ *
+ * The redux API for methods getState() and dispatch is exposed directly.
+ *
+ * The remaining redux API method subscribe() is not exposed, instead an observable is made available.
+ *
+ * The user can subscribe to store value changes using store.value.subscribe().
+ *
+ */
 
 export abstract class ReduxStore {
 
