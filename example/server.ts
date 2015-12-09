0da689943682655a0b209dba50987165e96aa2d9
Your Name <you@example.com>
2015-12-09 22:59:25 +0100

    adding asyn examples

--- example/server.ts
+++ example/server.ts
@@ -16,20 +16,20 @@ app.use(bodyParser.text());
 app.route('/todo')
     .get((req, res) => {
         console.log(JSON.stringify(todos));
-        setTimeout(() => res.send(todos), 2000);
+        res.send(todos);
     })
     .put((req, res) => {
         let json = req.body;
         let toggled = _.find(todos, (todo) => todo.id == json.id);
         toggled.completed = !toggled.completed;
         console.log(JSON.stringify(todos));
-        setTimeout(() => res.send(), 2000);
+        res.send();
     })
     .delete((req,res) => {
         console.log('removing todo with id = ' + req.query.id);
         todos = _.remove(todos,(todo) => todo.id != req.query.id );
         console.log(JSON.stringify(todos));
-        setTimeout(() => res.send(), 2000);
+        res.send();
     })
     .post((req, res) => {
         todos.push(req.body);
