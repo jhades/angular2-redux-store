005ccd937d4ae33c0c55e7eeb4e0b847c64e6046
Your Name <you@example.com>
2015-12-09 21:55:38 +0100

    added redux-logger

--- example/server.ts
+++ example/server.ts
@@ -16,24 +16,26 @@ app.use(bodyParser.text());
 app.route('/todo')
     .get((req, res) => {
         console.log(JSON.stringify(todos));
-        res.send(todos);
+        setTimeout(() => res.send(todos), 2000);
     })
     .put((req, res) => {
         let json = req.body;
         let toggled = _.find(todos, (todo) => todo.id == json.id);
         toggled.completed = !toggled.completed;
         console.log(JSON.stringify(todos));
-        res.send();
+        setTimeout(() => res.send(), 2000);
     })
     .delete((req,res) => {
         console.log('removing todo with id = ' + req.query.id);
         todos = _.remove(todos,(todo) => todo.id != req.query.id );
         console.log(JSON.stringify(todos));
+        setTimeout(() => res.send(), 2000);
         res.send();
     })
     .post((req, res) => {
         todos.push(req.body);
         console.log(JSON.stringify(todos));
+        setTimeout(() => res.send(), 2000);
         res.send();
     });
 
